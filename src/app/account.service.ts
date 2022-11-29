e7d0a86d5ba59c8a5aa82a774e32563fe62f1e99
KncskBenedek <koncsikbenedek@gmail.com>
2022-11-29 10:21:24 +0100

    111, service injection into services

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -1,3 +1,9 @@
+import { Injectable } from "@angular/core";
+import { LoggingService } from "./logging.service";
+
+@Injectable()
+
+
 export class AccountService{
     accounts = [
         {
@@ -14,11 +20,17 @@ export class AccountService{
         }
       ];
 
+      constructor(private loggingService: LoggingService){
+
+      }
+
       addAcount(name: string, status: string){
         this.accounts.push({name: name, status: status});
+        this.loggingService.logStatusChange(status);
       }
 
       updateStatus(id: number, newStatus: string){
         this.accounts[id].status = newStatus;
+        this.loggingService.logStatusChange(newStatus);
       }
 }
